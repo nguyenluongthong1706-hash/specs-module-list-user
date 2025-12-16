866c4aa648351050461cae7a4cfd63533ef921b1
thongnguyen003 <thong0855@gmail.com>
2025-12-16 16:22:25 +0700

    update API with MockAPI

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
-const API = "https://jsonplaceholder.typicode.com/users";
+// https://69411bf0686bc3ca8165a6ec.mockapi.io/users
+// https://jsonplaceholder.typicode.com/users
+const API = "https://69411bf0686bc3ca8165a6ec.mockapi.io/users";
 let users = [];
 let editingId = null;
 
+const tableBody   = document.getElementById("tableBody");
+const cardList    = document.getElementById("cardList");
+const modal       = document.getElementById("modal");
+const formTitle   = document.getElementById("formTitle");
+
+const nameUser    = document.getElementById("nameUser");
+const username    = document.getElementById("username");
+const email       = document.getElementById("email");
+const phone       = document.getElementById("phone");
+const website     = document.getElementById("website");
+
+const street      = document.getElementById("street");
+const suite       = document.getElementById("suite");
+const city        = document.getElementById("city");
+const zipcode     = document.getElementById("zipcode");
+const geoLat      = document.getElementById("geoLat");
+const geoLng      = document.getElementById("geoLng");
+
+const nameCompany = document.getElementById("nameCompany");
+const catchPhrase = document.getElementById("catchPhrase");
+const bs          = document.getElementById("bs");
+
 async function loadUsers() {
-    const res = await fetch(API);
-    users = await res.json();
-    render();
+    try {
+        const res = await fetch(API);
+        users = await res.json();
+        render();
+    } catch (err) {
+        alert(`Tải danh sách user thất bại! Lỗi: ${err.message}`);
+    }
+}
+
+async function addUserAPI(user) {
+    const res = await fetch(API, {
+        method:"POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    });
+    return await res.json();
+}
+
+async function updateUserAPI(user,id) {
+    const res = await fetch(`${API}/${id}`, {
+        method:"PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    });
+    return await res.json();
+}
+
+async function deleteUserAPI(id) {
+    const res = await fetch(`${API}/${id}`, {
+        method:"DELETE",
+    });
+    return await res.json();
 }
 
 function render() {
@@ -51,12 +104,20 @@ function openForm(user = null) {
     if (user) {
         formTitle.innerText = "Cập nhật user";
         editingId = user.id;
-        name.value = user.name;
+        nameUser.value = user.name;
         username.value = user.username;
         email.value = user.email;
+        website.value = user.website || "";
+        street.value = user.address?.street || "";
+        suite.value = user.address?.suite || "";
         city.value = user.address?.city || "";
+        zipcode.value = user.address?.zipcode || "";
+        geoLat.value = user.address?.geo?.lat || "";
+        geoLng.value = user.address?.geo?.lng || "";
         phone.value = user.phone;
-        company.value = user.company?.name || "";
+        nameCompany.value = user.company?.name || "";
+        catchPhrase.value = user.company?.catchPhrase || "";
+        bs.value = user.company?.bs || "";
     } else {
         formTitle.innerText = "Thêm user";
         editingId = null;
@@ -66,41 +127,96 @@ function openForm(user = null) {
 
 function closeForm() {
     modal.style.display = "none";
+    editingId = null;
 }
 
-function saveUser() {
+async function saveUser() {
+    if (!nameUser.value || !username.value) {
+        alert("Tên và usernam là bắt buộc");
+        return;
+    }
+    existUsername = users.find(u => u.username === username.value && u.id != editingId);
+    if (existUsername) {
+        alert("Username đã tồn tại, vui lòng chọn username khác");
+        return;
+    }
     if (editingId) {
-        const u = users.find(x => x.id === editingId);
-        u.name = name.value;
+        const u = users.find(x => x.id == editingId);
+        u.name = nameUser.value;
         u.username = username.value;
         u.email = email.value;
         u.phone = phone.value;
+        u.website = website.value;
+        u.address.street = street.value;
+        u.address.suite = suite.value;
         u.address.city = city.value;
-        u.company.name = company.value;
+        u.address.zipcode = zipcode.value;
+        u.address.geo.lat = geoLat.value;
+        u.address.geo.lng = geoLng.value;
+        u.company.name = nameCompany.value;
+        u.company.catchPhrase = catchPhrase.value;
+        u.company.bs = bs.value;
+        try{
+            const updated = await updateUserAPI(u, editingId);
+            users = users.map(user =>
+                user.id == editingId ? updated : user
+            );
+            alert("Cập nhật user thành công!");
+        }catch (err){
+            alert(`Cập nhật user thất bại! Lỗi: ${err.message}`);
+        }
+        
     } else {
-        users.unshift({
-        id: Date.now(),
-        name: name.value,
-        username: username.value,
-        email: email.value,
-        phone: phone.value,
-        address: { city: city.value },
-        company: { name: company.value }
-        });
+        const newUser = {
+            name: nameUser.value,
+            username: username.value,
+            email: email.value,
+            phone: phone.value,
+            website: website.value,
+            address: { 
+                street: street.value,
+                suite: suite.value,
+                city: city.value,
+                zipcode: zipcode.value,
+                geo: { 
+                    lat: geoLat.value, 
+                    lng: geoLng.value }
+            },
+            company: {
+                name: nameCompany.value,
+                catchPhrase: catchPhrase.value,
+                bs: bs.value
+            }  
+        };
+        try{
+            const createdUser = await addUserAPI(newUser);
+            users.push(createdUser);
+            alert("Thêm user thành công!");
+        }catch (err){
+            alert(`Thêm user thất bại! Lỗi: ${err.message}`);
+        }
     }
     closeForm();
-    render();
+    loadUsers();
 }
 
 function editUser(id) {
-    openForm(users.find(u => u.id === id));
+    const user = users.find(u => u.id == id);
+    console.log(users);
+    if (!user) return alert("User không tồn tại");
+    openForm(user);
 }
 
-function deleteUser(id) {
-    if (confirm("Xoá user?")) {
-        users = users.filter(u => u.id !== id);
-        render();
+async function deleteUser(id) {
+    if (confirm("Bạn có chắc chắn muốn xóa user này?")) {
+        try{
+            await deleteUserAPI(id);
+            users = users.filter(u => u.id != id);
+            render();
+            alert("Xóa user thành công!");
+        }catch (err){
+            alert(`Xóa user thất bại! Lỗi: ${err.message}`);
+        }
     }
 }
-
 loadUsers();
